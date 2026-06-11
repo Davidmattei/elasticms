11f2b2f74b6a1c227f200abf81fc0fe637214bf5
David mattei <d.mattei89@gmail.com>
2026-06-11 13:36:30 +0200

    feat(ems): add wysiwyg npm package

---
+++ package/wysiwyg/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    dts({ include: ['src'] }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // externe dependencies hier toevoegen (bijv. 'vue', 'react')
+      external: [],
+    },
+  },
+})
