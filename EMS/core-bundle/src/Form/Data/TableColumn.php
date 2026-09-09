80f7c6c3e51ed805a5a3630fde24bfe58dce1212
David mattei <d.mattei89@gmail.com>
2026-09-09 15:28:31 +0200

    fix: dataTable use translatableMessage

--- EMS/core-bundle/src/Form/Data/TableColumn.php
+++ EMS/core-bundle/src/Form/Data/TableColumn.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Form\Data;
 
 use EMS\CommonBundle\Common\Spreadsheet\SpreadsheetValidation;
-use EMS\CoreBundle\EMSCoreBundle;
 use EMS\CoreBundle\Form\Data\Condition\ConditionInterface;
 use Symfony\Component\Translation\TranslatableMessage;
 
@@ -57,7 +56,7 @@ class TableColumn
             return $this->titleKey;
         }
 
-        return new TranslatableMessage($this->titleKey, $this->transLabelOptions, EMSCoreBundle::TRANS_DOMAIN);
+        return new TranslatableMessage($this->titleKey, $this->transLabelOptions);
     }
 
     public function getAttribute(): string
